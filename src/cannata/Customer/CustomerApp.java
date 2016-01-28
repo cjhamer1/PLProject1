f2c0b2d2ef894fb9ca02c82b54da6dae56fb2c46
Cameron Hamer <cameronj.hamer@utexas.edu>
2016-01-28 17:52:52 -0600

    no message

---
+++ src/cannata/Customer/CustomerApp.java
@@ -0,0 +1,15 @@
+package cannata;
+
+public class Jan26Class {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        //System.out.println("Hello, World");
+        //int variableName = 3;
+        int i = 4;
+        String s = "some string";
+        //int m = i * s;
+        System.out.println("\tsome " + i + "\nstring");
+    }
+
+}
