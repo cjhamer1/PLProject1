b3bc361ced53613c5f9d79eac61c73b57cbd1ddb
Cameron Hamer <cameronj.hamer@utexas.edu>
2016-02-04 16:23:08 -0600

    while loop

--- src/hamer/customer/CustomerApp.java
+++ src/hamer/customer/CustomerApp.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class CustomerApp {
 
     public static void main(String[] args) {
+
+        String display = "y";
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter a customer number: ");
-        int n = reader.nextInt();
-        // from here we need to do something with the input
+
+        while(display == "y") {
+            System.out.println("Enter a customer number: ");
+            int n = reader.nextInt();
+            // from here we need to do something with the input
+
+
+
+            System.out.println("Display another customer? (y/n): ");
+            display = reader.nextLine();
+        }
+
     }
 
 }
--- src/hamer/customer/CustomerDB.java
+++ src/hamer/customer/CustomerDB.java
@@ -2,8 +2,10 @@ package hamer.customer;
 
 public class CustomerDB {
 
-    public static Customer getCustomer(int n) {
-        return n;
+    public static Customer getCustomer(int customerNumber) {
+        if(custoermNumber == 1001);
+
+
     }
 
 
