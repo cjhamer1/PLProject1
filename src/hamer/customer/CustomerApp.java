3983bd702a04977607db0147c36818c9a58338c0
Cameron Hamer <cameronj.hamer@utexas.edu>
2016-02-04 18:15:03 -0600

    Input fix

--- src/hamer/customer/CustomerApp.java
+++ src/hamer/customer/CustomerApp.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 public class CustomerApp {
 
     public static void main(String[] args) {
-
-        String display = "y";
+        boolean repeat = true;
+        System.out.println("Welcome to the Customer Viewer");
         Scanner reader = new Scanner(System.in);
 
-        while(display == "y") {
-            System.out.println("Enter a customer number: ");
-            int n = reader.nextInt();
+        while(repeat) {
+            System.out.println("\nEnter a customer number: ");
+            int n = Integer.parseInt(reader.nextLine());
             // from here we need to do something with the input
-
-
+            Customer c1 = CustomerDB.getCustomer(n);
+            if (c1 == null) {
+                System.out.println("\nThere is no customer number " + n + " in our records.\n");
+            } else {
+                System.out.println("\n" + c1.getNameAndAddress() + "\n");
+            }
 
             System.out.println("Display another customer? (y/n): ");
-            display = reader.nextLine();
+            String input = reader.nextLine();
+            if ("y".equals(input)) {
+                repeat = true;
+            } else {
+                repeat = false;
+            }
         }
-
     }
-
 }
--- src/hamer/customer/CustomerDB.java
+++ src/hamer/customer/CustomerDB.java
@@ -1,13 +1,33 @@
 package hamer.customer;
 
 public class CustomerDB {
-
     public static Customer getCustomer(int customerNumber) {
-        if(custoermNumber == 1001);
-
-
+        if (customerNumber == 1001) {
+            Customer c1 = new Customer();
+            c1.setName("Barbara White");
+            c1.setAddress("3400 Richmond Parkway #3423");
+            c1.setCity("Bristol");
+            c1.setState("CT");
+            c1.setPostalCode("06010");
+            return c1;
+        } else if (customerNumber == 1002) {
+            Customer c2 = new Customer();
+            c2.setName("Karl Vang");
+            c2.setAddress("327 Franklin Street");
+            c2.setCity("Edina");
+            c2.setState("MN");
+            c2.setPostalCode("55435");
+            return c2;
+        } else if (customerNumber == 1003) {
+            Customer c3 = new Customer();
+            c3.setName("Ronda Chavan");
+            c3.setAddress("518 Commanche Dr.");
+            c3.setCity("Greensboro");
+            c3.setState("NC");
+            c3.setPostalCode("27410");
+            return c3;
+        } else {
+            return null;
+        }
     }
-
-
-
 }
